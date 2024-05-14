f498a05f00078f6b38fc97cbc3a6c860375f2f32
chukwuebuka <emmanuel.henry71@gmail.com>
2024-05-14 11:16:58 +0100

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,21 @@ import "./Home.css";
 import './signUp.css'
 import './login.css'
 import './signIn.css'
-import NotFoundPage from "./pages/NotFoundPage";
+import Landing from "./component/landing";
+import Home from "./pages/home-page";
 import Signup from "./pagess/signUp";
 import Login from "./pagess/logIn";
 import Signin from "./pagess/signIn";
+import NotFoundPage from "./pages/NotFoundPage";
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/landing" element={<Landing />} />
-        <Route path="/Home" element={<Home />} /> */}
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/Home" element={<Home />} /> 
+        <Route path="/Signup" element={<Signup />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signin" element={<Signin />} />
         <Route path="*" element={<NotFoundPage />} />

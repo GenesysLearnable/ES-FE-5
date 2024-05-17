218d11165d4b2a37609f1460bf61759a079ce0c5
chukwuebuka <emmanuel.henry71@gmail.com>
2024-05-17 20:48:32 +0100

    added functions to call buttons

--- src/call/call.jsx
+++ src/call/call.jsx
@@ -22,6 +22,57 @@ import medics from "./call-image/Ambulance staff member emerges from the back of
 
 function Call() {
   const [query, setQuery] = useState("");
+  const [showAll, setShowAll] = useState(false);
+  const [callHistory, setCallHistory] = useState([
+    {
+      name: "Officer John",
+      number: "+2348045679876",
+      duration: "00:20:00",
+      img: medics,
+    },
+    {
+      name: "Medic James",
+      number: "+2349051180952",
+      duration: "00:20:00",
+      img: pilot2,
+    },
+    {
+      name: "Medic James",
+      number: "+2348045679876",
+      duration: "00:20:00",
+      img: pilot2,
+    },
+    {
+      name: "Medic James",
+      number: "+2348077141863",
+      duration: "00:20:00",
+      img: pilot2,
+    },
+    {
+      name: "Fireman Jake",
+      number: "+2348045679877",
+      duration: "00:10:00",
+      img: male,
+    },
+    {
+      name: "Officer Paul",
+      number: "+2348045679878",
+      duration: "00:15:00",
+      img: pilot1,
+    },
+    {
+      name: "Medic George",
+      number: "+2348045679879",
+      duration: "00:05:00",
+      img: front,
+    },
+    {
+      name: "Security Guard",
+      number: "+2348045679880",
+      duration: "00:25:00",
+      img: securityGuards,
+    },
+  ]);
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
@@ -33,13 +84,17 @@ function Call() {
     }
   };
 
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div>
       <nav>
         <ul>
           <li>
             <Link to="/Home">
-              <img id="call-logo" src={logo} alt="" />
+              <img id="call-logo" src={logo} alt="Logo" />
             </Link>
           </li>
         </ul>
@@ -56,9 +111,9 @@ function Call() {
         </div>
 
         <div className="call-nav-IMG-1">
-          <img id="call-bell-1" src={bell} alt="" />
-          <a href="">
-            <img src={portrait} alt="" />
+          <img id="call-bell-1" src={bell} alt="Bell" />
+          <a href="/">
+            <img src={portrait} alt="User" />
           </a>
         </div>
       </nav>
@@ -69,7 +124,7 @@ function Call() {
             <ul>
               <li className="call-location-inner-quick-section-1">
                 <Link to="/Home">
-                  <img src={frame} alt="" />{" "}
+                  <img src={frame} alt="Home" />{" "}
                   <span className="call-location-inner-quick-section-1-1">
                     Home
                   </span>
@@ -77,7 +132,7 @@ function Call() {
               </li>
               <li className="call-location-inner-quick-section-1">
                 <Link to="/Location">
-                  <img src={location} alt="" />{" "}
+                  <img src={location} alt="Location" />{" "}
                   <span className="call-location-inner-quick-section-1-1">
                     My Location
                   </span>
@@ -85,7 +140,7 @@ function Call() {
               </li>
               <li className="call-location-inner-quick-section-1">
                 <Link to="/">
-                  <img src={book} alt="" />{" "}
+                  <img src={book} alt="Resources" />{" "}
                   <span className="call-location-inner-quick-section-1-1">
                     Resources
                   </span>
@@ -93,7 +148,7 @@ function Call() {
               </li>
               <li className="call-location-inner-quick-section-1">
                 <Link to="/">
-                  <img src={phone} alt="" />{" "}
+                  <img src={phone} alt="Calls" />{" "}
                   <span className="call-location-inner-quick-section-1-1">
                     Calls
                   </span>
@@ -101,7 +156,7 @@ function Call() {
               </li>
               <li className="call-location-inner-quick-section-1 location-bottom-line">
                 <Link to="/">
-                  <img src={coins} alt="" />{" "}
+                  <img src={coins} alt="Donations" />{" "}
                   <span className="call-location-inner-quick-section-1-1">
                     Donations
                   </span>
@@ -111,11 +166,9 @@ function Call() {
             <span>&nbsp;&nbsp;</span>
             <div>
               <ul>
-                {" "}
-                {}
                 <li className="call-location-inner-quick-section-1 location-top-line">
                   <Link to="/">
-                    <img src={spanner} alt="" />{" "}
+                    <img src={spanner} alt="Settings" />{" "}
                     <span className="call-location-inner-quick-section-1-1">
                       Settings
                     </span>
@@ -123,7 +176,7 @@ function Call() {
                 </li>
                 <li className="call-location-inner-quick-section-1">
                   <Link to="/">
-                    <img src={login} alt="" />{" "}
+                    <img src={login} alt="Log Out" />{" "}
                     <span className="call-location-inner-quick-section-1-1">
                       Log Out
                     </span>
@@ -136,85 +189,49 @@ function Call() {
             <h1>Emergency service contacts</h1>
             <div className="call-quick-section-2">
               <div>
-                <img className="guards" src={securityGuards} alt="" />
-                <img className="guards icons-1" src={icons} alt="" />
-                <button id="guards">Create contact</button>
+                <img className="guards" src={securityGuards} alt="Security Guards" />
+                <img className="guards icons-1" src={icons} alt="Icon" />
+                <a href="tel:+1234567890"><button id="guards">Create contact</button></a>
               </div>
               <div>
-                <img className="male" src={male} alt="" />
-                <button id="male">Fireman Jake</button>
+                <img className="male" src={male} alt="Male Firefighter" />
+                <a href="tel:+1234567891"><button id="male">Fireman Jake</button></a>
               </div>
               <div>
-                <img className="pilot1" src={pilot1} alt="" />
-                <button id="pilot1">Officer Paul</button>
+                <img className="pilot1" src={pilot1} alt="Pilot 1" />
+                <a href="tel:+1234567892"><button id="pilot1">Officer Paul</button></a>
               </div>
               <div>
-                <img className="front" src={front} alt="" />
-                <button id="front">Medic George</button>
+                <img className="front" src={front} alt="Front View" />
+                <a href="tel:+1234567893"><button id="front">Medic George</button></a>
               </div>
             </div>
             <div className="call-history">
               <div>
                 <div className="inner-call-history">
                   <span id="history">Call History</span>
-                  <span id="view">view all</span>
+                  <span id="view" onClick={toggleShowAll}>
+                    {showAll ? "view less" : "view all"}
+                  </span>
                 </div>
-                <div id="amaka">
-                  <div className="inner-call-history-1">
-                    <img src={medics} alt="" className="contact-img" />
-                    <div className="contacts">
-                      <p>Officer John</p>
-                      <p className="nums">+2348045679876</p>
+                {callHistory.slice(0, showAll ? callHistory.length : 4).map((call, index) => (
+                  <div id="phone-calls" key={index}>
+                    <div className="inner-call-history-1">
+                      <img src={call.img} alt={call.name} className="contact-img" />
+                      <div className="contacts">
+                        <p>{call.name}</p>
+                        <p className="nums">{call.number}</p>
+                      </div>
+                      <div className="call-history-contacts">
+                        <p>Call Duration</p>
+                        <p className="nums">{call.duration}</p>
+                      </div>
+                      <a href={`tel:${call.number}`}>
+                        <img src={phone2} alt="Phone" className="contact-img-1" />
+                      </a>
                     </div>
-                    <div className="call-history-contacts">
-                      <p>Call Duration</p>
-                      <p className="nums">00:20:00</p>
-                    </div>
-                    <img src={phone2} alt="" className="contact-img-1" />
                   </div>
-                </div>
-                <div id="amaka">
-                  <div className="inner-call-history-1">
-                    <img src={pilot2} alt="" className="contact-img" />
-                    <div className="contacts">
-                      <p>Medic James</p>
-                      <p className="nums">+2348045679876</p>
-                    </div>
-                    <div className="call-history-contacts">
-                      <p>Call Duration</p>
-                      <p className="nums">00:20:00</p>
-                    </div>
-                    <img src={phone2} alt="" className="contact-img-1" />
-                  </div>
-                </div>
-                <div>
-                  <div className="inner-call-history-1">
-                    <img src={pilot2} alt="" className="contact-img" />
-                    <div className="contacts">
-                      <p>Medic James</p>
-                      <p className="nums">+2348045679876</p>
-                    </div>
-                    <div className="call-history-contacts">
-                      <p>Call Duration</p>
-                      <p className="nums">00:20:00</p>
-                    </div>
-                    <img src={phone2} alt="" className="contact-img-1" />
-                  </div>
-                </div>
-                <div>
-                  <div className="inner-call-history-1">
-                    <img src={pilot2} alt="" className="contact-img" />
-                    <div className="contacts">
-                      <p>Medic James</p>
-                      <p className="nums">+2348045679876</p>
-                    </div>
-                    <div className="call-history-contacts">
-                      <p>Call Duration</p>
-                      <p className="nums">00:20:00</p>
-                    </div>
-                    <img src={phone2} alt="" className="contact-img-1" />
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
@@ -225,3 +242,4 @@ function Call() {
 }
 
 export default Call;
+

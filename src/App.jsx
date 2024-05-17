9f8cc1eb39d8954a90e0848962dc50f9496eb6a6
cyberimmortal7 <tajuzieogu@gmail.com>
2024-05-17 14:24:46 +0300

    modified app.jsk

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 // import Landing from "./component/landing";
 // import Home from "./pages/home-page";
@@ -7,14 +6,17 @@ import "./Home.css";
 import './signUp.css'
 import './login.css'
 import './signIn.css'
+import './location.css'
+import './call.css'
 import Landing from "./component/landing";
 import Home from "./pages/home-page";
 import Signup from "./pagess/signUp";
 import Login from "./pagess/logIn";
 import Signin from "./pagess/signIn";
+import Location from './location/location';
 import NotFoundPage from "./pages/NotFoundPage";
 import ContactUs from './contactpage/contactus';
-
+import Call from './call/call';
 
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/Signin" element={<Signin />} />
         <Route path="/ContactUs" element={<ContactUs />} />
+        <Route path="/Location" element={<Location />} />
+        <Route path="/Call" element={<Call />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
